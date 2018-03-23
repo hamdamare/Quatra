ba89478b33f2fe2c7a60a111bd0c2f3466073ab4
hamdamare <hamare@ualberta.ca>
2018-03-23 16:05:09 -0600

    misc added

--- app/src/main/java/com/example/hamdamare/quatras/CategoriesActivity.java
+++ app/src/main/java/com/example/hamdamare/quatras/CategoriesActivity.java
@@ -15,87 +15,45 @@ public class CategoriesActivity extends AppCompatActivity {
     }
 
     //navigates to the technology page
-    public void technology() {
-        Button tech = findViewById(R.id.techBtn);
-        tech.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),TechnologyActivity.class);
-                startActivity(intent);
-
-            }
-        });
+    public void technology(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
 
     }
 
 
     //navigates to the food activity
-    public void food() {
-        Button food = findViewById(R.id.foodBtn);
-        food.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),FoodActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void food(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
 
     }
 
 
 
 
-    public void entertainment() {
-        Button entertainment = findViewById(R.id.entertainmentBtn);
-        entertainment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),EntertainmentActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void entertainment(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
 
     }
 
-    public void grocery() {
-        Button grocery = findViewById(R.id.groceryBtn);
-        grocery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),GroceryActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-    }
-
-    public void clothing() {
-        Button clothing = findViewById(R.id.clothingBtn);
-        clothing.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(getApplicationContext(),ClothingActivity.class);
-                startActivity(intent);
-
-            }
-        });
-
-    }
-
-    public void beauty() {
-        Button beauty = findViewById(R.id.beautyBtn);
-        beauty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),BeautyActivity.class);
-                startActivity(intent);
-            }
-        });
-
+    public void grocery(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
     }
 
 
+    public void clothing(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void beauty(View view) {
+        Intent intent = new Intent(this, GroceryActivity.class);
+        startActivity(intent);
+
+    }
 
 }
