dcd165de865d065e1b1072b950aa2c9056b5f667
Ikram Shire <ikramshire@Ikrams-MacBook-Air.local>
2018-03-23 16:41:37 -0600

    food act done

--- app/src/main/java/com/example/hamdamare/quatras/FoodActivity.java
+++ app/src/main/java/com/example/hamdamare/quatras/FoodActivity.java
@@ -1,7 +1,11 @@
 package com.example.hamdamare.quatras;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FoodActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class FoodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
     }
+     public void openM (View view){
+         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www4.mcdonalds.ca/coupons/"));
+         startActivity(browserIntent);
+
+     }
+    public void openB (View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://burgerking.ca/offers"));
+        startActivity(browserIntent);
+    }
+    public void openD (View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dairyqueen.com/ca-en/Promotions-CA/"));
+        startActivity(browserIntent);
+    }
+    public void openF (View view) {
+        Toast.makeText(FoodActivity.this, "15% Off With Valid Student ID At Participating Locations ", Toast.LENGTH_SHORT).show();
+    }
+    public void openS (View view) {
+        Toast.makeText(FoodActivity.this, "10% Off With Valid Student ID At Participating Locations ", Toast.LENGTH_SHORT).show();
+    }
+
 }
