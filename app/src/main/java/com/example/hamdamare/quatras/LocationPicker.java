d51d521c7643b1b0870d441d1ab068fc6a27e29d
hamdamare <hamare@ualberta.ca>
2018-03-23 21:48:39 -0600

    fixing location

--- app/src/main/java/com/example/hamdamare/quatras/LocationPicker.java
+++ app/src/main/java/com/example/hamdamare/quatras/LocationPicker.java
@@ -1,77 +1,21 @@
-package com.example.syn_tax;
+package com.example.hamdamare.quatras;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
- * LocationPickerActivity Class
- *
- * March 10, 2018
- *
- * ALLOWS the user to choose a location for a task of there's
- *
- *
+ * Created by hamdamare on 2018-03-23.
  */
-public class LocationPickerActivity extends AppCompatActivity {
 
-    int PLACE_LOCATION_REQUESTED = 1;
+public class LocationPicker extends AppCompatActivity {
 
-    TextView tvlocation;
+        int PLACE_LOCATION_REQUESTED = 1;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
-        tvlocation = (TextView) findViewById(R.id.tvlocation);
-
-    }
-
-    /**
-     * Allow the user to pick a location
-     * @param view
-     */
-    public void goLocationPicker(View view) {
-        //calling the place picker function
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-
-        try {
-            startActivityForResult(builder.build(LocationPickerActivity.this), PLACE_LOCATION_REQUESTED);
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
-        } catch (GooglePlayServicesRepairableException e) {
-            e.printStackTrace();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
         }
-    }
-
-    /**
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == PLACE_LOCATION_REQUESTED) {
-            if(resultCode == RESULT_OK) {
-                Place location = PlacePicker.getPlace(LocationPickerActivity.this,data);
-                tvlocation.setText(location.getAddress());
-
-            }
-
-        }
-
-
-    }
-
-
 
 
 }
